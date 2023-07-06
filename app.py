45ba51eebc68eb2546654ead38d305a41a383895
Bana143123 <118828247+Bana143123@users.noreply.github.com>
2023-07-06 16:34:36 +0000

    Created html css flask basic web

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
 app=Flask(__name__)
 
+JOBS=[
+  {
+    'id':1,
+    'job':'software developer',
+    'loc':'bangalore',
+    'SAL':'2,00,000'
+  },
+  {
+  'id':2,
+    'job':'Automation engineer',
+    'loc':'chennai',
+    'SAL':'2,50,000'
+  }
+]
 @app.route("/")
 def hello_world():
-  return "helloflask!"
+  return render_template("home.html",jobs=JOBS,company='JOVIAN')
+
+@app.route('/api/jobitem')
+def jobdetail():
+  return jsonify(JOBS)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
